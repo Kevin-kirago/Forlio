221e21cdea8219e8675a1bb803da5fce0f84331a
Kevin <tkevinkiragu@gmail.com>
2020-02-16 09:40:13 +0300

    scroll and smooth scroll implementation

---
+++ js/index.js
@@ -0,0 +1,29 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+	var currentscrollpos = window.pageYOffset;
+
+	if (prevScrollpos > currentscrollpos) {
+		document.getElementById("navbar").style.top = "0";
+	} else {
+		document.getElementById("navbar").style.top = "-100px";
+	}
+
+	prevScrollpos = currentscrollpos;
+};
+
+// animations (animate.css)
+function animateCSS(element, animationName, callback) {
+	const node = document.querySelector(element);
+	node.classList.add("animated", animationName);
+
+	function handleAnimationEnd() {
+		node.classList.remove("animated", animationName);
+		node.removeEventListener("animationend", handleAnimationEnd);
+
+		if (typeof callback === "function") callback();
+	}
+
+	node.addEventListener("animationend", handleAnimationEnd);
+}
+
+animateCSS(".scroll-indicator", "bounce");
